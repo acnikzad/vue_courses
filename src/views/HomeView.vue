de8532c9007806549a86fedd25927d6fefe86eb3
Aria Nikzad <a.c.nikzad@gmail.com>
2022-06-26 18:42:13 -0700

    displayed indexes in drop down input menu

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,56 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="row">
+      <label for="courses">Choose a Course: </label>
+      <select id="courses" name="courses">
+          <option v-for="course in courses">{{course.name}}</option>
+      </select>
+      <label for="students">Choose a Student: </label>
+      <select id="students" name="students">
+          <option v-for="student in students" :value= "student.id">{{student.first_name}} {{student.last_name}}</option>
+      </select>
+      <label for="teachers">Choose a Teacher: </label>
+      <select id="teachers" name="teachers">
+          <option v-for="teacher in teachers">{{teacher.first_name}} {{teacher.last_name}}</option>
+      </select>
+    </div>
+    <div class="row">
+
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      courses: [],
+      students: [],
+      teachers: [],
+      currentCourse: {},
+      currentStudent: {},
+      currentTeacher: {},
+
+    };
+  },
+
+  created: function() {
+    axios.get("/api/courses").then(response => {
+      this.courses = response.data;
+    });
+    axios.get("/api/students").then(response => {
+      this.students = response.data;
+    });
+    axios.get("/api/teachers").then(response => {
+      this.teachers = response.data;
+    });
+  },
+
+  methods: {
+
   }
 }
 </script>
